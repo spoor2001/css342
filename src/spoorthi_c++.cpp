8cc0d25be9ea8cc49d2e09a43fc04472d3f08c4c
spoorthi <spoorthi@spoorthi-VirtualBox>
2021-01-07 23:25:22 -0800

    new cpp

---
+++ src/spoorthi_c++.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : spoorthi_c++.cpp
+// Author      : Spoorthi Gowda
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#ifndef
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
